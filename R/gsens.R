424a1739cb2bec261beb8bc9fc39e1d35464e1bf
LeonardFrach <leonard.frach.20@ucl.ac.uk>
2022-04-29 18:30:49 +0100

    Description changes

--- R/gsens.R
+++ R/gsens.R
@@ -4,7 +4,7 @@ sapply(load.lib, require, character = TRUE) # Load libraries
 
 #' @title Adjusting for genetic confounding using PGS for the outcome
 
-#' Adjusting for genetic confounding in exposure-outcome associations using the polygenic score for the outcome.
+#' @description  Adjusting for genetic confounding in exposure-outcome associations using the polygenic score for the outcome.
 #' This is the recommended function for most scenarios, and the only function that has been extended to the multiple exposure case.
  
 #' @param data Either a data frame of raw data or a covariance/correlation matrix. 
@@ -243,7 +243,7 @@ gsensY = function(data,
 
 #' @title Adjusting for genetic confounding using PGS for the exposure
 
-#' Adjusting for genetic confounding in exposure-outcome associations using the polygenic score for the exposure.
+#' @description Adjusting for genetic confounding in exposure-outcome associations using the polygenic score for the exposure.
 
 #' @param rxy The observed phenotypic correlation between exposure X and outcome Y.
 #' @param rgx The observed correlation between the polygenic score for X and exposure X.
@@ -314,7 +314,7 @@ gsensX = function(rxy,
 
 #' @title Adjusting for genetic confounding using PGS for the exposure and the outcome
 
-#' Adjusting for genetic confounding in exposure-outcome associations using the polygenic score for the exposure and the outcome
+#' @description Adjusting for genetic confounding in exposure-outcome associations using the polygenic score for the exposure and the outcome
 
 #' @param rxy The observed phenotypic correlation between exposure X and outcome Y.
 #' @param rg1x The observed correlation between the polygenic score for X and exposure X.
